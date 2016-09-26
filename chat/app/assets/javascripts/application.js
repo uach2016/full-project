514de3e94d8407742f41def686dc15b407e432fb
UNAI ALBERTO CAMINO HERRERA <uacaminoherrera@gmail.com>
2016-09-26 09:36:43 +0100

    links ready

--- chat/app/assets/javascripts/application.js
+++ chat/app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
-//= require turbolinks
 //= require underscore
 //= require gmaps/google
 //= require google-instant-hangouts
